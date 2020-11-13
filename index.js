d734d36f9460ff097576b1788cd89d0ffbd23ad4
Papa-Sean <smsugrue@gmail.com>
2020-11-12 20:33:48 -0500

    index.js support files

---
+++ index.js
@@ -0,0 +1,6 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+
+const api = require('.api.js');
+const generateMarkdown = require('.generateMarkdown.js');
