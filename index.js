cf1f697ecdd2c91ca0f787cc59a784ad77bf3e2b
Papa-Sean <smsugrue@gmail.com>
2020-11-12 22:22:49 -0500

    github username

--- index.js
+++ index.js
@@ -4,3 +4,21 @@ const util = require('util');
 
 const api = require('.api.js');
 const generateMarkdown = require('.generateMarkdown.js');
+
+const questions = [
+    {
+        type: 'input',
+        message: "GitHub Username?",
+        name: 'username',
+        default: 'papa-sean-dev',
+        validate: function (answer){
+            if (answer.length < 1){
+                return console.log("GitHub username required");
+            }
+            return true;
+        }
+    },
+    {
+        
+    }
+]
