d744eba9b0879a726b0861a922221ff7ebb7a096
Papa-Sean <smsugrue@gmail.com>
2020-11-12 22:52:53 -0500

    generate markdown

--- index.js
+++ index.js
@@ -57,6 +57,64 @@ const questions = [
         }
     },
     {
-        
+        type:'input',
+        message: "Installation Instructions",
+        name: 'installation'
+
+
+    },
+    {
+        type:'input',
+        message:"Project Instructions / Examples of project in use",
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: "How can developer contribute?",
+        name:'contributing'
+    },
+    {
+        type: 'input',
+        message: "Tests written for your app / examples of how to run them",
+        name: 'tests'
+    },
+    {
+        type: 'list',
+        message:"Choose a license",
+        choices: ['MSU','Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+        name:'license'
     }
-]
+];
+
+function writeToFile(fileName, data){
+    fs.writeFile(fileName, data, err => {
+        if(err){
+            return console.log(err);
+        }
+        console.log("Success! README.me generated!")
+    });
+}
+
+const writeFileAsync = util.promisify(writeToFile);
+
+async function init(){
+    try{
+        const userResponses = await inquirer.prompt(questions);
+        console.log("Your responses: ", userResponses);
+        console.log("Thank you, fetching GitHub data next...");
+
+        const userInfo = await api.getUser(userResponses);
+        console.log("GitHub user info: ", userInfo);
+
+        console.log("Generating your README next...")
+        const markdown = generateMarkdown(userResponses, userInfo);
+        console.log(markdown);
+
+        await writeFileAsync('ExampleREADMES.md', markdown);
+
+    }catch (error){
+        console.log(error);
+    }
+};
+
+init()
